9322465f6debf05719cd5567cf685d83941f1e87
SamatarCodes <thewind07@protonmail.com>
2020-05-17 18:40:35 -0400

    changed button colors and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-blue': '#4285f4',
+        'button-bg-colors': '#f2f2f2',
+        'button-text-color': '#222',
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
